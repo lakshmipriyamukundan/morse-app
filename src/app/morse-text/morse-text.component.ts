eb5e5e82d08fd91fc1f7edd458edf9ed7539f976
devc271(deprecated) <lakshmipriya.m@cubettech.com>
2019-03-19 16:24:17 +0530

    component refactoring

--- src/app/morse-text/morse-text.component.ts
+++ src/app/morse-text/morse-text.component.ts
@@ -4,38 +4,8 @@ import { plainToMorse } from "../../assets/functions/converter";
 
 @Component({
   selector: "app-morse-text",
-  template: `
-    <div class="maindiv">
-      <div>
-        <p>Write your text</p>
-        <textarea
-          cols="50"
-          rows="8"
-          (input)="str = $event.target.value"
-        ></textarea>
-        <p>
-          <label>Operation : </label>
-          <select (change)="selectOperation($event.target.value)">
-            <option value="toText">--Morse to Text--</option>
-            <option value="toMorse">--Text to Morse--</option>
-          </select>
-        </p>
-        <p>
-          <button class="convert-button" (click)="submit()">
-            Submit
-          </button>
-        </p>
-        <p>
-          <textarea class="out-class" cols="50" rows="8" readonly>{{
-            otText
-          }}</textarea>
-        </p>
-      </div>
-    </div>
-  `,
-  styles: [
-    ".maindiv { display: flex; justify-content: center } .out-class {background-color: #ddd} .convert-button {background-color: #4CAF50; border: none; color: white}"
-  ]
+  templateUrl: "morse-text.html",
+  styleUrls: ["./morse-text.css"]
 })
 export class MorseTextComponent implements OnInit {
   constructor() {}
